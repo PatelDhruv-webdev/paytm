b87802032737830acc4c2f1e57744a8b750581e7
Patel Dhruv <dhruv3415@gmail.com>
2024-06-19 18:10:31 -0400

    getting started

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,19 @@
 const express = require("express");
+const rootRouter = require('./routes/index');
 
+const app = express();
+
+
+
+app.use('/api/v1', rootRouter);
+
+
+const PORT =3000 
+module.exports = {PORT}
+
+
+app.listen(PORT,() =>{
+    console.log("server is running")
+    
+})
 
